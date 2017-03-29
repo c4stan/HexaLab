b8c1eaacbff4be4c542d49f60b49777b0fb45420
Matteo Bracci <matteobracci1@gmail.com>
2017-03-29 15:48:08 +0200

    bugfixes, some js side code. still no rendering :(

--- js/hexalab_renderer.js
+++ js/hexalab_renderer.js
@@ -1,17 +1,14 @@
 "use strict";
 
-// ------------------------------------------------------------------------------------------------------
-// Globals
-// ------------------------------------------------------------------------------------------------------
-var g_scene, g_camera, g_renderer, g_controls, g_render_item, g_mesh, g_plane;
+var g_scene, g_camera, g_renderer, g_controls, g_mesh, g_plane, g_visualizer;
+var g_vbuffer, g_ibuffer;
 
-// ------------------------------------------------------------------------------------------------------
-// Init
-// ------------------------------------------------------------------------------------------------------
 function renderer_init() {
     g_scene = new THREE.Scene();
     var width = window.innerWidth;
     var height = window.innerHeight;
+
+    g_visualizer = new Module.Visualizer();
 
     g_renderer = new THREE.WebGLRenderer();
     g_renderer.setSize(width, height);
@@ -46,9 +43,7 @@ function renderer_init() {
     var background_color = new THREE.Color(0.15, 0.15, 0.15);
     g_renderer.setClearColor(background_color, 1);
 }
-// ------------------------------------------------------------------------------------------------------
-// Rendering
-// ------------------------------------------------------------------------------------------------------
+
 function renderer_render() {
     g_renderer.render(g_scene, g_camera);
 }
@@ -58,42 +53,69 @@ function renderer_animate() {
     renderer_render();
     requestAnimationFrame(renderer_animate);
 }
-// ------------------------------------------------------------------------------------------------------
-// Mesh
-// ------------------------------------------------------------------------------------------------------
-function renderer_set_mesh(HLmesh) {
-    if (g_mesh) g_mesh.delete();
-    g_mesh = HLmesh
-    
-    renderer_update_plane(1, 0, 0, 0.5);
-    renderer_update_mesh();
+
+function renderer_update_vbuffer() {
+    g_vbuffer = [];
+
+    var vertices_base = g_visualizer.get_vbuffer();
+    var vertices_size = g_visualizer.get_vbuffer_size() * 4 * 3;
+    for (var i = 0; i < vertices_size;) {
+        var f1 = Module.getValue(vertices_base + i, 'float');
+        i += 4;
+        var f2 = Module.getValue(vertices_base + i, 'float');
+        i += 4;
+        var f3 = Module.getValue(vertices_base + i, 'float');
+        i += 4;
+        var vertex = new THREE.Vector3(f1, f2, f3);
+        g_vbuffer.push(vertex);
+    }
 }
 
-function renderer_update_plane(nx, ny, nz, s) {
-    var normal = new Module.vec3(nx, ny, nz);
-    var d = g_mesh.get_diagonal_size();
-    var center_x = g_mesh.get_center_x();
-    var center_y = g_mesh.get_center_y();
-    var center_z = g_mesh.get_center_z();
-    var x = center_x + nx*(s*d - d/2);
-    var y = center_y + ny*(s*d - d/2)
-    var z = center_z + nz*(s*d - d/2);
-    var center = new Module.vec3(x, y, z);
-    g_plane = new Module.Plane(normal, center);
+function renderer_update_ibuffer() {
+    g_ibuffer = [];
+    
+    var indices_base = g_visualizer.get_ibuffer();
+    var indices_size = g_visualizer.get_ibuffer_size() * 4;
+    for (var i = 0; i < indices_size;) {
+        var i1 = Module.getValue(indices_base + i, 'i32');
+        i += 4;
+        var i2 = Module.getValue(indices_base + i, 'i32');
+        i += 4;
+        var i3 = Module.getValue(indices_base + i, 'i32');
+        i += 4;
+        var face = new THREE.Face3(i1, i2, i3);
+        g_ibuffer.push(face);
+    }
 }
+
 
 function renderer_update_mesh() {
-    // remove current renderable
-    g_scene.remove(g_render_item);
+    if (g_mesh) {
+        g_scene.remove(g_mesh);
+    }
 
-    // build an updated index buffer
-    g_mesh.make_ibuffer(g_plane);
+    var geometry = new THREE.Geometry();
+    geometry.vertices = g_vbuffer;
+    geometry.faces = g_ibuffer;
+    var material = new THREE.MeshLambertMaterial({ color: 0xeeccee, wireframe : false});
+    g_mesh = new THREE.Mesh(geometry, material);
+
+    g_scene.add(g_mesh);
+}
+
+
+/*
+function renderer_update_mesh() {
+    // remove current renderable
+    g_scene.remove(g_mesh);
 
     // build a new render item
-    var vertices_base = g_mesh.get_vbuffer();
-    var vertices_size = g_mesh.get_vertices_count() * 4 * 3;
-    var indices_base = g_mesh.get_ibuffer();
-    var indices_size = g_mesh.get_indices_count() * 4;
+    var vertices_base = g_visualizer.get_vbuffer();
+    var vertices_size = g_visualizer.get_vbuffer_size() * 4 * 3;
+    var indices_base = g_visualizer.get_ibuffer();
+    var indices_size = g_visualizer.get_ibuffer_size() * 4;
+
+    log(g_visualizer.get_ibuffer_size() + "indices \n");
 
     var geometry = new THREE.Geometry();
     for (var i = 0; i < vertices_size;) {
@@ -120,9 +142,10 @@ function renderer_update_mesh() {
     geometry.computeFaceNormals();
     //geometry.computeVertexNormals();
 
-    var material = new THREE.MeshLambertMaterial({ color: 0xeeccee, wireframe : false /*,side : THREE.DoubleSide*/ });
-    g_render_item = new THREE.Mesh(geometry, material);
+    var material = new THREE.MeshLambertMaterial({ color: 0xeeccee, wireframe : false });
+    g_mesh = new THREE.Mesh(geometry, material);
 
     // add the new render item to the scene
-    g_scene.add(g_render_item);
+    g_scene.add(g_mesh);
 }
+*/
