b47b6929d3b1f616bbb89445234b308db7fa9a3e
Matteo Bracci <matteobracci1@gmail.com>
2017-04-07 16:03:09 +0200

    -float3

--- cpp/include/visualizer.h
+++ cpp/include/visualizer.h
@@ -17,9 +17,9 @@ namespace HexaLab {
             js_vec3() : Eigen::Vector3f() {};
             js_vec3(const Eigen::Vector3f& v) : Eigen::Vector3f(v) {};
             js_vec3(const Eigen::Vector3f&& v) : Eigen::Vector3f(v) {};
-            float get_x() { return *(float*)this; }
-            float get_y() { return *((float*)this + 1); }
-            float get_z() { return *((float*)this + 2); }
+            float get_x() { return x(); }
+            float get_y() { return y(); }
+            float get_z() { return z(); }
         };
 
         struct ViewFace {
@@ -36,12 +36,10 @@ namespace HexaLab {
         };
 
     private:
-        using float3 = Eigen::Vector3f;
-
         Mesh mesh;
         AlignedBox3f mesh_aabb;
         Eigen::Hyperplane<float, 3> plane;
-        std::vector<float3> vbuffer;
+        std::vector<Vector3f> vbuffer;
         std::vector<ViewFace> faces;
         std::vector<ViewEdge> edges;
         std::vector<ViewVert> verts;
@@ -49,14 +47,14 @@ namespace HexaLab {
 
     public:
         void set_culling_plane(const Eigen::Hyperplane<float, 3>& plane) { this->plane = plane; }
-        void set_culling_plane(float3 normal, float3 position) { this->plane = Eigen::Hyperplane<float, 3>(normal, position); }
-        void set_culling_plane(float3 normal, float d) { this->plane = Eigen::Hyperplane<float, 3>(normal, d); }
-        void set_culling_plane(float nx, float ny, float nz, float x, float y, float z) { this->plane = Eigen::Hyperplane<float, 3>(float3(nx, ny, nz), float3(x, y, z)); }
+        void set_culling_plane(Vector3f normal, Vector3f position) { this->plane = Eigen::Hyperplane<float, 3>(normal, position); }
+        void set_culling_plane(Vector3f normal, float d) { this->plane = Eigen::Hyperplane<float, 3>(normal, d); }
+        void set_culling_plane(float nx, float ny, float nz, float x, float y, float z) { this->plane = Eigen::Hyperplane<float, 3>(Vector3f(nx, ny, nz), Vector3f(x, y, z)); }
         void set_culling_plane(float nx, float ny, float nz, float s) {
             float size = mesh_aabb.diagonal().norm();
-            float3 center = mesh_aabb.center();
-            float3 normal = float3(nx, ny, nz);
-            float3 pos = center + normal * (size * s - size / 2);
+            Vector3f center = mesh_aabb.center();
+            Vector3f normal = Vector3f(nx, ny, nz);
+            Vector3f pos = center + normal * (size * s - size / 2);
             set_culling_plane(normal, pos);
         }
 
@@ -68,7 +66,7 @@ namespace HexaLab {
         js_vec3 get_center() { return js_vec3(mesh_aabb.center()); }
     
         js_ptr get_vbuffer() { return (js_ptr)this->vbuffer.data(); }
-        size_t get_vbuffer_size() { return this->vbuffer.size() * sizeof(float3); }
+        size_t get_vbuffer_size() { return this->vbuffer.size() * sizeof(Vector3f); }
         js_ptr get_faces() { return (js_ptr)this->faces.data(); }
         size_t get_faces_size() { return this->faces.size() * sizeof(ViewFace); }
         js_ptr get_edges() { return (js_ptr)this->edges.data(); }
